b989459059e9a551ce440c3b880a7c05423ac4ae
fouqueta <aurfouquet@gmail.com>
2021-02-10 15:59:00 +0100

    Fonction deplace()

--- src/CasesSpeciales.java
+++ src/CasesSpeciales.java
@@ -1,10 +1,10 @@
 
 public class CasesSpeciales extends Cases {
 	
-	String evenement;
-	int transaction;
+	private String evenement;
+	private int transaction;
 	
-	public CasesSpeciales(String type, String evenement, int transaction) {
+	public CasesSpeciales(String evenement, int transaction) {
 		super("Speciale");
 		this.evenement = evenement;
 		this.transaction = transaction;		
--- src/Proprietes.java
+++ src/Proprietes.java
@@ -1,11 +1,11 @@
 
 public class Proprietes extends Cases {
 	
-	int prixAchat;
-	int loyer;
-	String couleur;
+	private int prixAchat;
+	private int loyer;
+	private String couleur;
 	
-	public Proprietes(String type, int prixAchat, int loyer, String couleur) {
+	public Proprietes(int prixAchat, int loyer, String couleur) {
 		super("Propriete");
 		this.prixAchat = prixAchat;
 		this.loyer = loyer;
