811d7c19667c0ebb8ce9b6255e14767874977378
Marie Rochier <marie.rochier92@gmail.com>
2021-02-17 19:56:02 +0100

    Gestion de l'achat/vente de propriétés

--- src/Joueur.java
+++ src/Joueur.java
@@ -12,10 +12,6 @@ public class Joueur{
     	pion = new Pion(0);
     	argent = 1500;
     	proprietes = null;
-		this.reponse = new Scanner(System.in);
-    }
-    
-    public Joueur() { //FIXME: jsp si je le met direct dans le 1er constructeur
     	this.reponse = new Scanner(System.in);
     }
     
@@ -29,13 +25,52 @@ public class Joueur{
     		return questionDes();
     	}
     }
-
-	public Pion getPion() {
-		return pion;
+   
+   //Getters
+	public Pion getPion() { return pion; }
+	
+	public String getNom() { return nom; }
+	
+	public int getArgent() { return argent; }
+	
+	//Gestion de l'achat/vente de propriétés
+	public boolean decision_achat() {
+		System.out.println(this.getNom() + ", si vous souhaitez acheter la propriété, tapez \"oui\" sinon \"non\".");
+    	String s = reponse.next();
+	    if(s.equals("oui")) {
+	    	return true;
+    	}
+	    else if(s.equals("non")) {
+	    	return false;
+	    }
+	    else {
+    		System.out.println("Erreur de frappe. Tapez \"oui\" ou \"non\".");
+    		return decision_achat();
+    	}
 	}
 	
-	public String getNom() {
-		return nom;
+	public void achat_effectue(int prix) {
+		this.argent = this.argent - prix;
+	}
+	
+	public void vente_effectuee(int prix) {
+		this.argent = this.argent + prix;
+	}
+	
+	public boolean decision_vente(Joueur proprietaire) {
+		System.out.println(proprietaire.getNom() + ", souhaitez-vous vendre cette propriété à " + this.getNom() + "?");
+		System.out.println(proprietaire.getNom() + ", si vous souhaitez vendre la propriété, tapez \"oui\" sinon \"non\".");
+    	String s = reponse.next();
+	    if(s.equals("oui")) {
+	    	return decision_achat();
+    	}
+	    else if(s.equals("non")) {
+	    	return false;
+	    }
+	    else {
+    		System.out.println(proprietaire.getNom() + ", souhaitez-vous vendre cette propriété à " + this.getNom() + "?");
+    		return decision_vente(proprietaire);
+    	}
 	}
 
 }
