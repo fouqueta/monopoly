3ecf3c88670249371a884680e87d3f83096cdae7
Marie Rochier <marie.rochier92@gmail.com>
2021-02-17 20:04:09 +0100

    Replace Proprietes.java

--- src/Proprietes.java
+++ src/Proprietes.java
@@ -15,17 +15,33 @@ public class Proprietes extends Cases {
 		this.nom = nom;
 		this.proprietaire = null;
 	}
+	
+	//Getters
+	public int getPrix() { return this.prixAchat; }
+	
+	public int getLoyer() { return this.loyer; }
+	
+	public String getCouleur() { return this.couleur; }
+	
+	public String getNom() { return this.nom; }
+	
+	public Joueur getProprietaire() { return this.proprietaire; }
 
-	@Override
-	public String getNom() {
-		return nom;
+	//Setters
+	public void setPrix(int prix) { this.prixAchat = prix; }
+	
+	public void setLoyer(int loyer) { this.loyer = loyer; }
+	
+	public void setProprietaire(Joueur nouveau_proprietaire) { this.proprietaire = nouveau_proprietaire; }
+	
+	//toString()
+	public String toString() {
+		return("Prix d'achat: " + this.prixAchat + " , Loyer actuel: " + this.loyer + " , Couleur: " + this.couleur + " , Nom: " + this.nom);
 	}
-
-	public int getPrix(){
-		return this.prixAchat;
-	}
-
-	public Joueur getProp(){
-		return this.proprietaire;
+	
+	//Vérification
+	public boolean est_Libre() {
+		if(this.proprietaire == null) { return true; }
+		return false;
 	}
 }
