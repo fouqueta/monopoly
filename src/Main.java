d4ffa4001198095a060f0f6216bf050150f00dea
fouqueta <aurfouquet@gmail.com>
2021-02-17 20:59:58 +0100

    Liaison lancer_de_des() deplace()

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 	
     public static void main(String[] args){
         Jeu j = new Jeu();
-        j.affiche();
-        j.deplace(j.getJoueurs()[0].getPion(), 6);
-        j.affiche();
+        j.debutTour();
+        //j.affiche();
+        //j.deplace(j.getJoueurs()[0].getPion(), 6);
+        //j.affiche();
         
     }
     
