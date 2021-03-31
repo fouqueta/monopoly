df069cdd85d2cdae661a05fa5514f9f1a7516622
Marie Rochier <marie.rochier92@gmail.com>
2021-03-31 20:36:55 +0200

    Cases speciales (prison, taxes, impots)

--- src/main/java/application/Controleur.java
+++ src/main/java/application/Controleur.java
@@ -22,23 +22,60 @@ public class Controleur {
 	
 	//Lancer de des
 	void controleur_lancer(int[] des, int curseur) {
-		System.out.println("Curseur: " + curseur);
-		Pion p = jeu.getJoueurs()[curseur].getPion();
-		int depart = p.getPosition();
-		jeu.deplace_IG(p, des);
-		vue.changement_argent(curseur);
-		int arrivee = p.getPosition();
-		vue.changement_position_pion(curseur, depart, arrivee);
+		//System.out.println("Curseur: " + curseur);
 		
-		Cases case_actuelle = jeu.getPlateau().getCases(arrivee);
-		if(case_actuelle.getType().equals("Propriete")) {
-			Proprietes propriete_actuelle = (Proprietes) jeu.getPlateau().getCases(arrivee);
-			if(!(propriete_actuelle.est_Libre()) && vue.getTabProprietaires(arrivee) != curseur
-				&& propriete_actuelle.coloree()){
-			jeu.loyer(propriete_actuelle);
+		if(jeu.getJoueurs()[curseur].isEnPrison() && 
+		(des[0] == des[1] || jeu.getJoueurs()[curseur].getNbToursPrison() == 1)) {
+			System.out.println("Vous etes libre.");
+			jeu.getJoueurs()[curseur].setEnPrison(false);
+		}
+		
+		else if(jeu.getJoueurs()[curseur].isEnPrison()) {
+			int tour_restant = jeu.getJoueurs()[curseur].getNbToursPrison();
+			System.out.println("Vous restez en prison pour encore " + tour_restant + " tours.");
+			jeu.getJoueurs()[curseur].setNbToursPrison(tour_restant-1);
+		}
+		
+		else if(!(jeu.getJoueurs()[curseur].isEnPrison())){
+			Pion p = jeu.getJoueurs()[curseur].getPion();
+			int depart = p.getPosition();
+			jeu.deplace_IG(p, des);
 			vue.changement_argent(curseur);
-			vue.changement_argent(vue.getTabProprietaires(arrivee));
-			System.out.println("Loyer payé.");
+			int arrivee = p.getPosition();
+			vue.changement_position_pion(curseur, depart, arrivee);
+		
+			Cases case_actuelle = jeu.getPlateau().getCases(arrivee);
+			if(case_actuelle.getType().equals("Propriete")) {
+				Proprietes propriete_actuelle = (Proprietes) jeu.getPlateau().getCases(arrivee);
+				if(!(propriete_actuelle.est_Libre()) && vue.getTabProprietaires(arrivee) != curseur
+				&& propriete_actuelle.coloree()){
+					jeu.loyer(propriete_actuelle);
+					vue.changement_argent(curseur);
+					vue.changement_argent(vue.getTabProprietaires(arrivee));
+					System.out.println("Loyer paye.");
+				}
+			}
+		
+			//Cases speciales (prison, taxe, impots)
+			else if(case_actuelle.getType().equals("Speciale")) {
+				switch(case_actuelle.getNom()) {
+					case "Impots revenu" :
+						break;
+					
+					case "Taxe de luxe" :
+						jeu.getJoueurs()[curseur].ajout( ((CasesSpeciales)case_actuelle).getTransaction() );
+						vue.changement_argent(curseur);
+						System.out.println("Vous avez paye " + Math.abs(((CasesSpeciales)case_actuelle).getTransaction()) + "e.");
+						break;
+					
+					case "Aller prison" :
+						System.out.println("Vous etes en prison.");
+						jeu.getJoueurs()[curseur].setEnPrison(true);
+						jeu.getJoueurs()[curseur].setNbToursPrison(3);
+						p.setPosition(10);
+						vue.changement_position_pion(curseur, arrivee, 10);
+						break;
+				}
 			}
 		}
 	}
