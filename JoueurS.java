ccdea00ffc5a3e69c6aabdf386a6dce4ba7e226f
CUNHA TEIXEIRA jose <cours.miguel@gmail.com>
2021-04-14 22:38:06 +0200

    Meilleure prise en charge de la déconnection avant que la partie se lance

--- JoueurS.java
+++ JoueurS.java
@@ -1,11 +1,14 @@
 package serveur.monopoly;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class JoueurS extends Thread{
     
@@ -15,6 +18,7 @@ class JoueurS extends Thread{
     ArrayList<JoueurS> list;
     boolean pret = false;
     static boolean lance = false;
+    boolean running = true;
 
     JoueurS(Socket socket, ArrayList<JoueurS> list) {
         this.socket = socket;
@@ -26,9 +30,9 @@ class JoueurS extends Thread{
         try{
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-            nom = br.readLine();
-            System.out.println(nom);
-            while(true){
+            //nom = br.readLine();
+            //System.out.println(nom);
+            while(running){
                 String action = br.readLine();
                 String info = br.readLine();
                 System.out.println(action);
@@ -90,6 +94,21 @@ class JoueurS extends Thread{
                 break;
             case "start":
                 pret = true;
+                nom = info;
+                break;
+            case "close":
+                try {
+                    socket.close();
+                    ServeurMonopoly.list.remove(this);
+                    list.forEach(j -> {
+                        j.list.remove(this);
+                    });
+                    running = false;
+                    this.interrupt();
+                } catch (IOException ex) {
+                    Logger.getLogger(JoueurS.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
                 break;
             default:
                 sendToAllClientsNotSender(action, info);
