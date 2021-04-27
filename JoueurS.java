bc17dcc685f3b45c4aa2ee2e10ee4e759b2c965a
CUNHA TEIXEIRA jose <cours.miguel@gmail.com>
2021-04-27 15:41:09 +0200

    Refonte serveur

--- JoueurS.java
+++ JoueurS.java
@@ -7,22 +7,34 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class JoueurS extends Thread{
     
-    private String nom;
+    private ServeurMonopoly serveur;
     private final Socket socket;
     private PrintWriter pw;
-    private final ArrayList<JoueurS> list;
     private boolean pret = false;
-    static boolean lance = false;
     private boolean running = true;
+    
+    private String nom;
+    private int position;
+    private int argent;
+    private int[] proprietes;
+    private boolean enPrison;
+    private boolean cartePrison;
+    private int nbToursPrison;
+    private boolean faillite;
+    
 
-    JoueurS(Socket socket, ArrayList<JoueurS> list) {
+    JoueurS(Socket socket, ServeurMonopoly s) {
         this.socket = socket;
-        this.list = list;
+        this.serveur = s;
+        position = 0;
+        argent = 10000;
+        proprietes = new int[0];
     }
     
     //Getters
@@ -32,11 +44,6 @@ class JoueurS extends Thread{
     
     public boolean getPret(){
         return pret;
-    }
-    
-    //Enleve un joueur de la liste de joueurs
-    public void removePlayer(JoueurS j){
-        list.remove(j);
     }
     
     //Envoi un message
@@ -57,14 +64,13 @@ class JoueurS extends Thread{
                 System.out.println(action);
                 System.out.println(info);
                 build_message(action, info);
-                if(!lance){
-                    tousPret();
+                if(!serveur.estLance()){
+                    serveur.tousPret();
                 }
-                
             }
         }
         catch(IOException e){
-            if(lance) sendToAllClientsNotSender("deco", this.getNom());
+            if(serveur.estLance()) sendToAllClientsNotSender("deco", this.getNom());
             this.closeClient();
             System.out.println(e);
         }
@@ -72,14 +78,14 @@ class JoueurS extends Thread{
     
     //Envoie le message à tout les clients
     private void sendToAllClients(String action, String info){
-        list.forEach(s -> {
+        serveur.getList().forEach(s -> {
             s.sendMsg(action, info);
         });
     }
     
     //Envoie le message à tout les clients sauf l'envoyeur 
     private void sendToAllClientsNotSender(String action, String info){
-        list.forEach(s -> {
+        serveur.getList().forEach(s -> {
             if(!s.getNom().equals(nom)){
                 s.sendMsg(action, info);
             }
@@ -105,32 +111,94 @@ class JoueurS extends Thread{
                 
             }
             case "close" -> closeClient();
+            case "achat" -> {
+                achatCase(Integer.parseInt(info));
+                sendToAllClientsNotSender(action, info);
+            }
+            /*case "tire carte" -> {
+                String carte = serveur.tireCarteChanceCommu(info);
+                this.sendMsg("carte tiree", carte + "-" + info);
+            }*/
+            case "lancer des" ->{
+                int[] des = this.lancer_de_des();
+                this.sendMsg("lancer des", des[0] + "-" + des[1]);
+            }
+            case "deplace" -> {
+                String[] temp = info.split("-");
+                this.position = Integer.parseInt(temp[2]);
+                this.argent = Integer.parseInt(temp[3]);
+                this.enPrison = temp[4].equals("true") ? true: false;
+                this.nbToursPrison = Integer.parseInt(temp[5]);
+                this.faillite = temp[6].equals("true") ? true: false;
+                this.cartePrison = temp[7].equals("true") ? true: false;
+                sendToAllClientsNotSender(action, info);
+            }
+            case "fin tour" -> {
+                serveur.setCurseur(Integer.parseInt(info));
+                sendToAllClientsNotSender(action, info);
+                System.out.println(serveur);
+            }
+            case "vente a joueur" -> {
+                String[] temp = info.split("-");
+                venteCase(Integer.parseInt(temp[0]), Integer.parseInt(temp[2]));
+                serveur.achatCase(temp[1], Integer.parseInt(temp[2]));
+                sendToAllClientsNotSender(action, info);
+            }
+            case "vendre" -> {
+                String[] temp = info.split("-");
+                venteCase(Integer.parseInt(temp[2]), Integer.parseInt(temp[1]));
+                sendToAllClientsNotSender(action, info);
+            }
+            case "defis gagnant" -> {
+                String[] t = info.split("-");
+                int loyerEnJeu = Integer.parseInt(t[3]);
+
+		if(t[0].equals("joueur")) { //Rembourse le loyer au joueur gagnant.
+                    this.argent = this.argent + loyerEnJeu;
+		}
+		else if(t[0].equals("proprio")) { //Joueur paye deux fois le loyer, il l'a deja paye une fois donc seulement une autre fois encore.
+                    loyer(loyerEnJeu,t[4]);
+		}
+                sendToAllClientsNotSender(action, info);
+            }
+            case "loyer" -> {
+                String[] temp = info.split("-");
+                loyer(Integer.parseInt(temp[0]),temp[1]);
+            }
             default -> sendToAllClientsNotSender(action, info);
         }
     }
     
-    //Verifie si tout les joueurs sont prêts et renvoie la liste des noms de joueurs
-    private void tousPret(){
-        boolean b = true;
-        String noms = "" ;
-        for(JoueurS s: list){
-            if(!s.getPret()){
-                b = false;
-                
-            }else{
-                if(!noms.equals("")){
-                    noms = noms + "-" + s.getNom();
-                }else{
-                    noms = s.getNom();
-                }
+    public void achatCase(int prix){
+        int[] temp = new int[this.proprietes.length+1];
+        System.arraycopy(this.proprietes, 0, temp, 0, this.proprietes.length);
+        temp[this.proprietes.length] = this.position;
+        this.proprietes = temp;
+        this.ajoutArgent(-prix);
+    }
+    
+    private void venteCase(int num, int prix){
+        int[] temp = new int[this.proprietes.length-1];
+        int j = 0;
+        for(int i: this.proprietes){
+            if(i!=num){
+                temp[j] = i;
+                j++;
             }
         }
-        System.out.println(noms);
-        if(list.size()>=2 && b){
-            sendToAllClients("start", noms);
-            lance = true;
-        }
+        this.proprietes = temp;
+        this.ajoutArgent(prix);
     }
+    
+    public void loyer(int loyer, String nom){
+        ajoutArgent(-loyer);
+        serveur.loyer(loyer, nom);
+    }
+    
+    private void deplacePos(String info) {
+        this.position = Integer.parseInt(info);
+    }
+    
     
     //Ferme le client correctement
     private void closeClient(){
@@ -138,10 +206,7 @@ class JoueurS extends Thread{
             pw.println("close");
             pw.println("");
             socket.close();
-            ServeurMonopoly.removePlayer(this);
-            list.forEach(j -> {
-                j.removePlayer(this);
-            });
+            serveur.removePlayer(this);
             running = false;
 
             this.interrupt();
@@ -151,9 +216,33 @@ class JoueurS extends Thread{
     }
 
     private boolean pseudoNonPresent(String info) {
-        for(JoueurS j: list){
-            if(j!=this && j.getNom().equals(info)) return false;
+        for(JoueurS j: serveur.getList()){
+            //System.out.println(j + " " + this + " " + j.getNom() + " " + j.socket + " " + this.socket);
+            if(j!=this && j.getNom()!=null && j.getNom().equals(info)) return false;
         }
-        return true;
+        return !info.contains("-");
+    }
+
+    void ajoutArgent(int loyer) {
+        this.argent = this.argent + loyer;
+    }
+    
+    public String toString(){
+        String rep = this.nom + ": \nArgent : " + this.argent + "\nPosition : " + this.position + "\nProprietes : ";
+        for(int i: this.proprietes){
+            rep = rep + i + " ";
+        }
+        rep = rep + "\nEn prison :" + this.enPrison + "\nNb de Tour : " + this.nbToursPrison + "\nCarte Prison : " + this.cartePrison + "\nEn Faillite" + this.faillite + "\n";
+        return rep;
+    }  
+    
+    public int[] lancer_de_des() {
+	int[] des = new int[2];
+	Random aleatoire = new Random();
+	for(int i = 0; i<des.length; i++) {
+            int intervalle = 1 + aleatoire.nextInt(7-1);
+            des[i] = intervalle;
+	}
+	return des;
     }
 }
--- ServeurMonopoly.java
+++ ServeurMonopoly.java
@@ -1,40 +1,150 @@
 package serveur.monopoly;
 
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 
-public class ServeurMonopoly {
+public class ServeurMonopoly{
 
-    private static ArrayList<JoueurS> list = new ArrayList<>();
-    
-    //Main qui attend les connections
-    public static void main(String[] args) {
-        try{
-            ServerSocket server=new ServerSocket(666);
-            while(true){
-                Socket socket=server.accept();
-                //TODO: verifier que la partie n'est pas lancé
-                if(list.size()<7){
-                    JoueurS serv=new JoueurS(socket, list);
-                    list.add(serv);
-                    serv.start();
-                    System.out.println(socket);
-                }else{
-                    socket.close();
-                }
-            }   
-        }
-        catch(Exception e){
-            System.out.println(e);
-            e.printStackTrace();
-        }
+    private final ArrayList<JoueurS> list;
+    private boolean lance;
+    private int curseur;
+    /*private String[] cartesChance;
+    private String[] cartesCommu;
+    private Scanner scan;*/
+
+    public ServeurMonopoly() {
+        this.list = new ArrayList<>();
+        this.lance = false;
+        this.curseur = 0;
+        //init_cartes();
     }
     
+    /*public void init_cartes() {
+    	new_scan("cartes.csv");
+    	cartesChance = new String[16];
+    	cartesCommu = new String[16];
+    	int i = 0;
+        scan.nextLine(); //Pour sauter la ligne des titres des categories
+        while (scan.hasNextLine()) {
+        	String cartes = scan.nextLine();
+        	String[] attributs = cartes.split(";"); //tab de taille 4, voir cartes.csv
+        	attributs[1] = attributs[1].replace("\\n", "\n");
+            switch (attributs[0]) {
+            	case "chance":
+            		cartesChance[i] =  attributs[2];
+            		break;
+            	case "commu":
+            		cartesCommu[i%16] = attributs[2];
+            		break;
+            }
+        	i++;
+        }
+    	scan.close();
+    }
+    
+    public void new_scan(String fichier) {
+    	try {
+    		scan = new Scanner(new File(fichier), "UTF-8");
+    	}
+    	catch(Exception e) {
+    		System.out.println("Erreur lors d ouverture fichier:");
+    		e.printStackTrace();
+    		System.exit(1);
+    	}
+    }*/
+    
+    public String toString(){
+        String rep = "Curseur : " + curseur + "\n";
+        for(JoueurS j: list){
+            rep = rep + j + "\n";
+        }
+        return rep;
+    }
+    
+    public void setCurseur(int i){
+        this.curseur = i;
+    }
+    
+    public ArrayList<JoueurS> getList(){
+        return list;
+    }
+    
+    public void add(JoueurS j){
+        list.add(j);
+    }
+    
+    public int nbJ(){
+        return list.size();
+    }
+    
+    public boolean estLance(){
+        return lance;
+    }
+        
     //Eneleve un joueur de la liste
-    static void removePlayer(JoueurS j){
+    public void removePlayer(JoueurS j){
         list.remove(j);
+        if(list.isEmpty()) lance = false;
     }
+    
+    //Verifie si tout les joueurs sont prêts et envoie la liste des noms de joueurs aux clients si tous sont prets
+    public void tousPret(){
+        boolean b = true;
+        String noms = "" ;
+        for(JoueurS s: list){
+            if(!s.getPret()){
+                b = false;
+            }else{
+                if(!noms.equals("")){
+                    noms = noms + "-" + s.getNom();
+                }else{
+                    noms = s.getNom();
+                }
+            }
+        }
+        System.out.println(noms);
+        if(list.size()>=2 && b){
+            sendtoAllClients("start", noms);
+            lance = true;
+        }
+    }
+    
+    private void sendtoAllClients(String action, String info){
+        list.forEach(j -> {
+            j.sendMsg(action, info);
+        });
+    }
+
+    void achatCase(String string, int prix) {
+        list.forEach(j -> {
+            if(j.getNom().equals(string)){
+                j.achatCase(prix);
+            }
+        });
+    }
+
+    void loyer(int loyer, String nom) {
+         list.forEach(j -> {
+            if(j.getNom().equals(nom)){
+                j.ajoutArgent(loyer);
+            }
+        });
+    }
+    
+    /*public String tireCarteChanceCommu(String caseC) {
+    	Random rand = new Random();
+	int alea = rand.nextInt(16);
+	String carte = null;
+	if (caseC.equals("chance")) {
+            carte = this.cartesChance[alea];
+	}
+	else if (caseC.equals("cmmu")) {
+            carte = this.cartesCommu[alea];
+	}
+	return carte;
+    }*/
     
 }
